0908ae97f2b4d1afbbf666058fdbc3fcb697bbf6
Hoffmann-Ferreira <andreihf@outlook.com>
2022-08-01 18:04:23 -0300

    feat: home page aside structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -37,7 +37,40 @@ const Home = () => {
           </div>
         </section>
       </div>
-      <aside></aside>
+      <aside>
+        <header>
+          <h2>Order number</h2>
+          <div>
+            <button>Pick up on the spot</button>
+            <button>Delivery</button>
+          </div>
+        </header>
+        <div>
+            <div>
+                <h3>item</h3>
+                <h3>the amount</h3>
+                <h3>price</h3>
+            </div>
+            <div className="checkout-cars-container">
+                <div> Card Checkout </div>
+                <div> Card Checkout </div>
+                <div> Card Checkout </div>
+            </div>
+        </div>
+        <div>
+            <div>
+                <p>discount</p>
+                <p>R$0.00</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <p>total</p>
+                <p>R$0.00</p>
+            </div>
+            <button>continue to payment</button>
+        </div>
+      </aside>
     </div>
   );
 };
