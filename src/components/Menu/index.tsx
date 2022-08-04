0b4ff552de4c88d0bed6e344fb18965f2b68c834
Hoffmann-Ferreira <andreihf@outlook.com>
2022-08-03 23:58:48 -0300

    feat: steld-components of  menu itens

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,26 @@
-import * as Styled from "./styles"
-import logo from "../../assets/images/logo.png"
-import  { HomeIcon, EditIcon, LogoutIcon } from "../../assets/icons/index"
-
+import * as Styled from "./styles";
+import logo from "../../assets/images/logo.png";
+import { HomeIcon, SettingsIcon, LogoutIcon } from "../../assets/icons/index";
 
 const Menu = () => {
   return (
     <Styled.MenuContainer>
-      <img src= {logo} alt="logo from site" />
-      <div>
-        <HomeIcon />
-        <EditIcon />
-      </div>
-      <LogoutIcon/>
+      <img src={logo} alt="logo from site" />
+      <nav>
+        <Styled.Menuitem>
+        <button>
+          <HomeIcon />
+        </button>
+        </Styled.Menuitem>
+        <Styled.Menuitem>
+        <button>
+          <SettingsIcon />
+        </button>
+        </Styled.Menuitem>
+      </nav>
+      <Styled.MenuLogout>
+        <LogoutIcon />
+      </Styled.MenuLogout>
     </Styled.MenuContainer>
   );
 };
--- src/components/Menu/styles.tsx
+++ src/components/Menu/styles.tsx
@@ -6,4 +6,41 @@ export const MenuContainer = styled.div`
   border-radius: 18px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  padding-top: 1.25rem;
+  box-sizing: border-box;
+  nav{
+    width: 100%
+  }
+  img {
+    width: 7rem;
+    margin-bottom: 1rem;
+  }
+`;
+
+export const Menuitem = styled.div `
+  border: solid red;
+  width: 6rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px 0px 0px 12px;
+  margin-bottom: 1.5rem;
+  margin-left: 0.2rem;
+  background-color: #FFFFFF;
+  cursor: pointer;
+`;
+
+export const MenuButoon = styled.button `
+`;
+
+export const MenuLogout = styled.button `
+  border: solid red;
+  width: 6rem;
+  height: 5rem;
+  cursor: pointer;
+  margin-top: 16rem;
+  margin-left: 0.5rem;
+  background-color: #FFFFFF
 `;
