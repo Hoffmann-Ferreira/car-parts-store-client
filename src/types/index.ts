c85166973d860c3bb0bb1d62a40ff22eea4f5754
Hoffmann-Ferreira <andreihf@outlook.com>
2022-08-07 12:09:14 -0300

    chore: added interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,47 @@
 export interface Product {
-    id: string;
-    name: string;
-    description: string;
-    price: number;
-    image: string;
-    createdAt: Date;
-    updatedAt: Date;
-    categoryId: string;
-};
+  id?: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  categoryId: string;
+}
 
 export interface Category {
-    id: string;
-    name: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id?: string;
+  name: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface Favorite {
+  id?: string;
+  creatAt?: Date;
+  userId: string;
+  productName: string;
+}
+
+export interface Order {
+  id?: string;
+  createdAt?: Date;
+  userId: string;
+}
+
+export interface OrderToProduct {
+  id?: string;
+  productId: string;
+  orderId: string;
+  quantity: number;
+  createdAt?: Date;
 }
