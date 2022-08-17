b9a900d61fb1596568adadfdc9e9754127327821
Hoffmann-Ferreira <andreihf@outlook.com>
2022-08-17 15:57:27 -0300

    feat: login only with authentication for user login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,33 +5,44 @@ import Button from "../../components/Button";
 import { Dispatch, SetStateAction, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 interface LoginProps {
   setLogged: Dispatch<SetStateAction<boolean>>;
 }
 
 const Login = ({ setLogged }: LoginProps) => {
-const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const handleLogin = () => {
-    if(email !=="" && password !== "") {
-        setLogged(true);
-        navigate("/")
-    } else {
-       toast.error("fill in the fields login!", {
-        icon: "❌",
-        style: {
-          borderRadius: "10px",
-          background: "#333",
-          color: "#fff",
-        },
-      }) 
-    }
+    if (email !== "" && password !== "") {
+      const data = {
+        email,
+        password,
+      };
 
-    
+      return axios
+        .post("https://car-parts-store-api.herokuapp.com/auth/login", data)
+        .then((res) => {
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("user", JSON.stringify(res.data.user));
+          setLogged(true);
+          navigate("/");
+        })
+        .catch(() => {
+          toast.error("incorrect username or password!", {
+            icon: "❌",
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+            },
+          });
+        });
+    }
   };
 
   return (
